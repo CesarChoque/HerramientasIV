4a865d452b9e8f7a6d843a42a7b036c96940fe04
User <User@LENOVO-LICE>
2022-12-28 08:13:17 -0500

    Nuevo proyecto

---
+++ H42022/src/h42022/H42022.java
@@ -0,0 +1,13 @@
+
+package h42022;
+
+public class H42022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
